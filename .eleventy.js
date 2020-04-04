bd94c5258bb28828657284306c7d4a00ea4c3740
Geoff-Balfre <geoff@balfre.com>
2020-04-04 20:50:29 +0100

    major update

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,13 @@
-
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/img");
-  eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
 
   return {
     passthroughFileCopy: true,
     dir: {
       input:     'src',
-      output:    'dist',
-      data:      '_data',
-      includes:  '_includes',
-      layouts:   '_layouts'
+      output:    'dist'
     }
   }
 
